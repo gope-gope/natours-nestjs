9780322b4d817e9136592580c348de2b88a1e930
gope-gope <georgigeorgiew97@gmail.com>
2026-01-16 17:09:18 +0200

    fix: missing module

--- src/tours/tours.module.ts
+++ src/tours/tours.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { UsersModule } from 'src/users/users.module';
+
 import { ToursController } from './tours.controller';
 import { TourSchema, Tour } from './tours.schema';
 import { ToursService } from './tours.service';
@@ -8,6 +10,7 @@ import { ToursService } from './tours.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Tour.name, schema: TourSchema }]),
+    UsersModule,
   ],
   controllers: [ToursController],
   providers: [ToursService],
